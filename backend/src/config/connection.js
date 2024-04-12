d003af40e071b940a3f30ec2939219746f6a834b
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-12 10:17:29 -0400

    connect to database

---
+++ backend/src/config/connection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connectDatabase = async () => {
+    const dbURI = process.env.URI || "mongodb://localhost:27017"
+    await mongoose.connect(dbURI);
+    console.log(`[${new Date().toISOString()}] MongoDB connected successfully at ${dbURI.split('@').pop()}`);
+    return mongoose.connection;
+};
+
+export default connectDatabase;

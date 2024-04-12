71fa1173f1f20967ad5e140b47b0482c4c9066a5
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-12 13:31:38 -0400

    update URI for local

--- backend/src/config/connection.js
+++ backend/src/config/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDatabase = async () => {
-    const dbURI = process.env.URI || "mongodb://localhost:27017"
+    const dbURI = process.env.URI || "mongodb://localhost:27017/econome"
     await mongoose.connect(dbURI);
     console.log(`[${new Date().toISOString()}] MongoDB connected successfully at ${dbURI.split('@').pop()}`);
     return mongoose.connection;

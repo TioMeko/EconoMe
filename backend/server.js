7f2f031ee17dd66623152d698ef31153fea1c782
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-12 13:32:41 -0400

    add connectivity automation with seeding

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,38 @@
 import "dotenv/config";
 import express from "express";
 import connectDatabase from "./src/config/connection.js";
+import seedDatabase from "./src/models/seed.js";
+import { User, Account } from "./src/models/index.js";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get("/", async (req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json(err));
+});
+
 const startServer = async () => {
   try {
     await connectDatabase();
+    console.log(
+      `[${new Date().toISOString()}] Connected to database, now seeding...`
+    );
+    await seedDatabase();
+    console.log(
+      `[${new Date().toISOString()}] Seeding completed, starting server...`
+    );
     app.listen(PORT, () => {
-      console.log(`[${new Date().toISOString()}] API server running on port ${PORT}`);
+      console.log(
+        `[${new Date().toISOString()}] API server running on port ${PORT}`
+      );
     });
   } catch (error) {
-    console.error(`[${new Date().toISOString()}] Failed to connect to the database:`, error);
+    console.error(
+      `[${new Date().toISOString()}] Failed to connect to the database or seed data:`,
+      error
+    );
     process.exit(1);
   }
 };

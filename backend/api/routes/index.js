8f50448aae5f666aaca419d57f55254b79f2d532
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-16 09:23:51 -0400

    update imports and use for savings and transactions

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import userRoutes from "./userRoutes.js";
 import accountRoutes from "./accountRoutes.js";
 import budgetRoutes from "./budgetRoutes.js";
-// import savingsRoutes from "./savingsRoutes.js";
-// import transactionRoutes from "./transactionRoutes.js";
+import savingsRoutes from "./savingsRoutes.js";
+import transactionRoutes from "./transactionRoutes.js";
 
 const router = Router();
 
-router.use('/users', userRoutes);
-router.use('/accounts', accountRoutes);
+router.use("/users", userRoutes);
+router.use("/accounts", accountRoutes);
 router.use("/budgets", budgetRoutes);
+router.use("/savings", savingsRoutes);
+router.use("/transaction", transactionRoutes);
 
 export default router;

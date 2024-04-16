97610f3ff51b018fa09685becba91d884d47f8ec
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-16 08:48:36 -0400

    add a controller for budgets

---
+++ backend/api/controllers/budgetController.js
@@ -0,0 +1,86 @@
+import { Budget } from "../models/index.js";
+
+// Create a new budget
+const createBudget = async (req, res) => {
+  try {
+    const newBudget = new Budget(req.body);
+    await newBudget.save();
+    console.log(`[${new Date().toISOString()}] New budget has been created.`);
+    res.status(201).send(newBudget);
+  } catch (error) {
+    // 400 Error if missing params
+    res.status(400).send({
+      message: "Failed to create budget due to missing or invalid fields.",
+      errors: error.errors,
+    });
+  }
+};
+
+// Get all budgets
+const getAllBudgets = async (req, res) => {
+  try {
+    const budgets = await Budget.find({});
+    res.send(budgets);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// Get a single budget by id
+const getBudget = async (req, res) => {
+  try {
+    const budget = await Budget.findById(req.params.id);
+    if (!budget) {
+      res.status(400).send({ message: "Budget not found" });
+    }
+    res.send(budget);
+  } catch (error) {
+    res.status(500).send({
+      error: "An error occurred while fetching the budget",
+    });
+  }
+};
+
+// Update an budget by id
+const updateBudget = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["category", "limit", "startDate", "endDate"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const budget = await Budget.findById(req.params.id);
+    if (!budget) {
+      return res.status(404).send();
+    }
+    // Iterates over each field name in updates and sets the new value from req.body to budget
+    updates.forEach((update) => (budget[update] = req.body[update]));
+    await budget.save();
+    res.send(budget);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Delete an budget by id
+const deleteBudget = async (req, res) => {
+  try {
+    const budget = await Budget.findByIdAndDelete(req.params.id);
+    if (!budget) {
+      return res.status(404).send();
+    }
+    res.send(budget);
+    console.log(`[${new Date().toISOString()}] Budget has been deleted.`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Failed to delete budget", error: error.message });
+  }
+};
+
+export { createBudget, getAllBudgets, getBudget, updateBudget, deleteBudget };

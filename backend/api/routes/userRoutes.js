8dcfd5cc45465cea5218802c36d40b6790dd96bb
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-22 09:58:37 -0400

    adjust routes for user registration and login

--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUser, createUser, updateUser, deleteUser } from "../controllers/userController.js";
+import { getUser, createUser, updateUser, deleteUser, loginUser } from "../controllers/userController.js";
 
 const router = Router();
 
 router.get("/:id", getUser);
-router.post("/", createUser);
+router.post("/register", createUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
+router.post("/login", loginUser);
 
 export default router;

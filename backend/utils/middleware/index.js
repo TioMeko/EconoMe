00339a8d86eec8dcaa8a9b49c654c11014a504fb
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-23 07:26:22 -0400

    add index file for easier export/import of middleware

---
+++ backend/utils/middleware/index.js
@@ -0,0 +1,5 @@
+import errorHandle from "./errorHandle.js"
+import requestLogging from "./requestLog.js"
+import authenticateToken from "./auth.js"
+
+export { errorHandle, requestLogging, authenticateToken };

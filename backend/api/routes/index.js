00071d6bdf15460d706011d67af101a110a80bac
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-04-23 06:52:52 -0400

    add authentication to specific routes

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { authenticateToken } from "../../middleware/middleware.js"
 import userRoutes from "./userRoutes.js";
 import accountRoutes from "./accountRoutes.js";
 import budgetRoutes from "./budgetRoutes.js";
@@ -8,9 +9,9 @@ import transactionRoutes from "./transactionRoutes.js";
 const router = Router();
 
 router.use("/users", userRoutes);
-router.use("/accounts", accountRoutes);
-router.use("/budgets", budgetRoutes);
-router.use("/savings", savingsRoutes);
-router.use("/transaction", transactionRoutes);
+router.use("/accounts", authenticateToken, accountRoutes);
+router.use("/budgets", authenticateToken, budgetRoutes);
+router.use("/savings", authenticateToken, savingsRoutes);
+router.use("/transactions", authenticateToken, transactionRoutes);
 
 export default router;
--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { authenticateToken } from "../../middleware/middleware.js"
 import { getUser, createUser, updateUser, deleteUser, loginUser } from "../controllers/userController.js";
 
 const router = Router();
 
-router.get("/:id", getUser);
+router.get("/:id", authenticateToken, getUser);
 router.post("/register", createUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.put("/:id", authenticateToken, updateUser);
+router.delete("/:id", authenticateToken, deleteUser);
 router.post("/login", loginUser);
 
 export default router;
